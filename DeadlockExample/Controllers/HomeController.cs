fcdbfea668cdd86d3e1e0257da4466947240a456
Derek Pittsinger <derekpitt@gmail.com>
2015-03-27 12:49:09 -0500

    more

--- DeadlockExample/Controllers/HomeController.cs
+++ DeadlockExample/Controllers/HomeController.cs
@@ -1,13 +1,32 @@
 ﻿using Foundatio.Storage;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace DeadlockExample.Controllers
 {
+    internal static class FileStorageExts
+    {
+        public static bool SaveObjectConfigAwaitFalse<T>(this IFileStorage storage, string path, T data)
+        {
+            return Task.Run(() => storage.SaveObjectAsyncConfigAwaitFalseAsync(path, data)).Result;
+        }
+
+        public static async Task<bool> SaveObjectAsyncConfigAwaitFalseAsync<T>(this IFileStorage storage, string path, T data, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            string json = JsonConvert.SerializeObject(data);
+            return await storage.SaveFileAsync(path, new MemoryStream(Encoding.UTF8.GetBytes(json)), cancellationToken).ConfigureAwait(false);
+        }
+
+    }
+
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -29,8 +48,20 @@ namespace DeadlockExample.Controllers
         public async Task<ActionResult> NoDeadlock()
         {
             var fs = new FolderFileStorage(System.IO.Path.GetTempPath());
-
             await fs.SaveObjectAsync("whammy", "ok");
+
+            return View();
+        }
+
+        public ActionResult NoDeadlockConfigAwait()
+        {
+            var fs = new FolderFileStorage(System.IO.Path.GetTempPath());
+
+            // damn..  still deadlocks with config await false
+            fs.SaveObjectConfigAwaitFalse("whammy", "ok");
+
+            // testing to see if using configwait false removes context??
+            System.Console.WriteLine(Request.UserAgent);
             return View();
         }
     }
