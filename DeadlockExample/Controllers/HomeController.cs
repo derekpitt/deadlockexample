87645ff2999675845c194a9c44efbcce8261d5f3
Derek Pittsinger <derekpitt@gmail.com>
2015-03-27 12:16:00 -0500

    deadlocking

---
+++ DeadlockExample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Foundatio.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeadlockExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult Deadlock()
+        {
+            var fs = new FolderFileStorage(System.IO.Path.GetTempPath());
+
+            fs.SaveObject("whammy", "ok");
+            return View();
+        }
+
+
+        public async Task<ActionResult> NoDeadlock()
+        {
+            var fs = new FolderFileStorage(System.IO.Path.GetTempPath());
+
+            await fs.SaveObjectAsync("whammy", "ok");
+            return View();
+        }
+    }
+}
---
+++ DeadlockExample/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DeadlockExample.Startup))]
+
+namespace DeadlockExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
